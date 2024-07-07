aa0903c72c89f5652bda0b1b6200960082de41a1
Pedro Markun <pedro@markun.com.br>
2024-07-07 09:58:07 -0300

    tools implementada, busca de projetos na cmsp

--- static/tools.js
+++ static/tools.js
@@ -1,6 +1,26 @@
 //import cmspAnalyzeProject from '../tools/cmsp.js';
 
-export const toolsTODO = [
+export async function cmspAnalyzeProject(tipo, ano, numero) {
+  await (new BroadcastChannel("activity")).postMessage({
+    command: 'play_text',
+    text: "Vou acessar agora o site da Câmara, baixar o projeto e analisar. Guenta ai!"
+  });
+  const response = await fetch('tools/cmspAnalyzeProject', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        "tipo" : "0",
+        "ano" : ano,
+        "numero" : numero
+    })
+});
+  return response;
+}
+
+
+export const tools = [
   {
     name: "cmspAnalyzeProject",
     description: "Analyze a legislative project from the São Paulo City Council and provide insights and voting recommendation",
@@ -28,8 +48,6 @@ export const toolsTODO = [
   }
 ];
 
-export const tools = [];
-
 export const toolDefinitions = tools.map(tool => ({
   type: "function",
   function: {
@@ -42,7 +60,7 @@ export const toolDefinitions = tools.map(tool => ({
 export const toolResponse = async (toolName, parameters) => {
   const tool = tools.find(t => t.name === toolName);
   if (tool) {
-    return await tool.handler(parameters);
+    return await tool.handler(JSON.parse(parameters));
   }
   throw new Error(`No tool found with name ${toolName}`);
 };
